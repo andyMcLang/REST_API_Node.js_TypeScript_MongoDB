14cfe7e54f836e086e4580e9b0d428195c491f88
Andy Lang <djdreviz@gmail.com>
2025-07-08 15:11:26 +0300

    1:03:31 done!

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from "express";
+import { validatePassword } from "../service/user.service";
+import { createSession } from "../service/session.service";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // Validate the user's password
+  const user = await validatePassword(req.body);
+
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+
+  if (!("_id" in user)) {
+    return res.status(500).send("User object missing _id");
+  }
   // create a session
+  const session = createSession(
+    user._id as string,
+    req.get("user-agent") || ""
+  );
   // create an access token
   // create a refresh token
   // return access & refresh tokens
