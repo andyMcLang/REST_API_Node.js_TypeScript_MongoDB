3c712322c8765858a4b1dfb5c7f00b979567e185
Andy Lang <djdreviz@gmail.com>
2025-07-14 14:48:12 +0300

    14:10 / 55:42 done!

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,14 @@
-import express from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
-import routes from "./routes";
-import deserializeUser from "./middleware/deserializeUser";
 import cors from "cors";
+import createServer from "./utils/server";
 
 console.log("Mongo URI:", config.get<string>("dbUri"));
 
 const port = config.get<number>("port");
-const app = express();
+
+const app = createServer();
 
 app.use(
   cors({
@@ -17,12 +16,6 @@ app.use(
     credentials: true, // jos käytät evästeitä (ei pakollinen JWT:lle)
   })
 );
-
-// Middlewaret
-app.use(express.json());
-app.use(deserializeUser);
-
-routes(app);
 
 app.listen(port, async () => {
   logger.info(`Appi on kaynnissa osoitteessa http://localhost:${port}`);
