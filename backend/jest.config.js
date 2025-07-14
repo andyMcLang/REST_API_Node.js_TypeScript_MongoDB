2c6cb184c0b41321cc38098f357427df61a7700f
Andy Lang <djdreviz@gmail.com>
2025-07-14 13:43:04 +0300

    4:35 done!

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,8 +4,13 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
+  testMatch: ["**/**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  // clearMocks: true,
 };
